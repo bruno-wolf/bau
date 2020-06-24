f6d8f56251fcc49c1254dfa6765ce69c8ebc92b4
bruno-wolf <bruno.m.lobato@gmail.com>
2020-06-24 13:01:19 +0100

    item nav - frontend dev

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,4 +1,8 @@
-// carousel
+/*
+ *
+ * carousel
+ * 
+ */
 let carrousel = document.querySelector('.carousel');
 let settings = {
 	cellAlign: 'left',
@@ -7,22 +11,89 @@ let settings = {
 };
 new Flickity(carrousel, settings);
 
-// nav
+/*
+ *
+ * item-nav
+ * 
+ */
+// ON CLICK
+// if this button has .select						// already open
+// // close();
+// else 																// not open
+// // buttons -> remove .select
+// // handlers -> add .hide
+// // this button -> add .select
+// // this handler -> remove .hide
+// // nav -> add .show
+
+// ON CLOSE = close();
+// buttons -> remove .select
+// handlers -> add .hide
+// nav -> remove .show
 let nav = document.querySelector('#item nav');
 let btnClose = nav.querySelector('.drawer .close');
+let btnsNav = Array.from(nav.querySelectorAll('.btn-group .btn-icon'));
 let btnComment = nav.querySelector('.btn-icon.comment');
-let commentHandler = nav.querySelector('.comment-handler');
+let btnShare = nav.querySelector('.btn-icon.share');
+let handlers = Array.from(nav.querySelectorAll('.handler'));
+let commentHandler = nav.querySelector('.comment.handler');
+let shareHandler = nav.querySelector('.share.handler');
 
-// close nav
+function close(show) {
+	btnsNav.forEach((element) => {
+		element.classList.remove('selected');
+	});
+	handlers.forEach((element) => {
+		element.classList.add('hide');
+	});
+	if (show === 'all') {
+		nav.classList.remove('show');
+	}
+}
+
+btnsNav.forEach((element) => {
+	element.addEventListener('click', function() {
+		// already open
+		if (this.classList.contains('selected')) {
+			close('all');
+		} else {
+			// not open
+			close('part');
+			this.classList.add('selected');
+			handlers.forEach((element) => {
+				if (this.dataset.trigger === element.dataset.handler) {
+					element.classList.remove('hide');
+				}
+			});
+			nav.classList.add('show');
+		}
+	});
+});
+
 btnClose.addEventListener('click', () => {
-	btnComment.classList.remove('selected');
-	commentHandler.classList.add('hide');
-	nav.classList.remove('show');
+	close('all');
 });
 
-// trigger comments
-btnComment.addEventListener('click', function() {
-	this.classList.toggle('selected');
-	commentHandler.classList.toggle('hide');
-	nav.classList.toggle('show');
-});
+/*
+ *
+ * copy share link to clipboard
+ * * @https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
+ * 
+ */
+function copy() {
+	var copyText = document.querySelector('#url');
+	copyText.select();
+	document.execCommand('copy');
+
+	try {
+		let msgHandler = document.querySelector('.share.handler p');
+		let successful = document.execCommand('copy');
+		let msg =
+			successful ? 'copied' :
+			'not copied';
+		msgHandler.innerHTML = 'The link was ' + msg;
+	} catch (err) {
+		msgHandler.innerHTML = 'Oops, unable to copy';
+	}
+}
+document.querySelector('#copy').addEventListener('click', copy);
--- item.php
+++ item.php
@@ -2,6 +2,14 @@
   require('includes/header.php');
   ?>
   <main>
+    <section id="check-new" class="hide">
+      <h1>Your New Collectible is Ready!</h1>
+      <p>Take a quick look to the info you added and check if it’s all OK or need some changses</p>
+      <div>
+        <button class="btn">Add to my collection</button>
+        <button class="btn dark">Edit</button>
+      </div>
+    </section>
     <section id="item">
       <div class="carousel">
         <div class="carousel-cell">
@@ -11,7 +19,7 @@
           <img src="media/figures/yoda_02.jpg" alt="" class="no-d">
         </div>
       </div>
-      <div class="info">
+      <div class="info hide">
         <div class="title">
           <h1>Yoda Sixth Scale Figure</h1>
           <p>Sideshow Collectibles | Star Wars</p>
@@ -37,7 +45,28 @@
               <use href="#i-cross" />
             </svg>
           </button>
-          <div class="comment-handler hide">
+          <!-- info -->
+          <div class="info handler hide" data-handler="info">
+            <div class="title">
+              <h1>Yoda Sixth Scale Figure</h1>
+              <p>Sideshow Collectibles | Star Wars</p>
+            </div>
+            <div class="desc">
+              <div>
+                <div class="btn-profile" aria-label="toggle navigation">
+                  <figure>
+                    <img src="media/users/bruno-lobato.jpg" alt="user">
+                  </figure>
+                </div>
+              </div>
+              <div>
+                <p><b>mr.wolf</b> Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod.</p>
+                <span class="date">20:00 13 jun 20</span>
+              </div>
+            </div>
+          </div>
+          <!-- comments -->
+          <div class="comment handler hide" data-handler="comment">
             <div class="comment-list">
               <div class="comment-item">
                 <div>
@@ -68,35 +97,47 @@
             </div>
             <form action="#" method="POST">
               <input type="text" name="comment" id="comment" placeholder="add a comment" aria-label="add a comment">
-              <button class="btn-icon">
+              <button class="btn-icon" aria-label="send comment">
                 <svg>
                   <use href="#i-arrow" />
                 </svg>
               </button>
             </form>
           </div>
+          <!-- share -->
+          <div class="share handler hide" data-handler="share">
+            <h2>Show This Precious To The World</h2>
+            <p>Just copy to your computer's clipboard</p>
+            <input type="text" id="url" value="https://bau.com/od8sadmw" aria-label="collectible url">
+            <button class="btn" id="copy">copy link</button>
+          </div>
         </div>
         <div class="btn-group">
-          <button class="btn-icon fav <?php if ($item['fav'] == 1) {
-                                        echo 'selected';
-                                      } ?>" aria-label="add to favourite">
+          <button class="btn-icon" data-trigger="info" aria-label="about this collectible">
+            <svg>
+              <use href="#i-info" />
+            </svg>
+          </button>
+          <button class="btn-icon <?php if ($item['fav'] == 1) {
+                                    echo 'selected';
+                                  } ?>" data-trigger="fav" aria-label="add to favourites">
             <svg>
               <use href="#i-bookmark" />
             </svg>
           </button>
-          <button class="btn-icon like <?php if ($item['like'] == 1) {
-                                          echo 'selected';
-                                        } ?>" aria-label="like">
+          <button class="btn-icon <?php if ($item['like'] == 1) {
+                                    echo 'selected';
+                                  } ?>" data-trigger="like" aria-label="like this collectible">
             <svg>
               <use href="#i-like" />
             </svg>
           </button>
-          <button class="btn-icon comment" aria-label="share">
+          <button class="btn-icon" data-trigger="comment" aria-label="check comments">
             <svg>
               <use href="#i-comments" />
             </svg>
           </button>
-          <button class="btn-icon share" aria-label="share">
+          <button class="btn-icon" data-trigger="share" aria-label="share this collectible">
             <svg>
               <use href="#i-share" />
             </svg>
